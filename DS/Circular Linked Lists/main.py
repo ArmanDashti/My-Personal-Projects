f740a54a0587e06d57f71f86b29f301c98b8c080
arman.dashti.97@gmail.com <arman.dashti.97@gmail.com>
2019-05-19 19:08:15 +0430

    CLL started

---
+++ DS/Circular Linked Lists/main.py
@@ -0,0 +1,9 @@
+from node import Node
+from cll import CircularLinkedList
+
+cllist = CircularLinkedList()
+cllist.append("C")
+cllist.append("D")
+cllist.prepend("B")
+cllist.prepend("A")
+cllist.print_list()
